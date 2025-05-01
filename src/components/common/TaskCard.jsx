848a9e7c23c1e229d769fa334348cf4f2820dedd
Mahmud-Dev63828 <mahmudul.hasan63828@gmail.com>
2025-05-01 18:59:33 +0600

    task action ui done

--- src/components/common/TaskCard.jsx
+++ src/components/common/TaskCard.jsx
@@ -1,44 +1,54 @@
-import React, { useState } from 'react'
-import { PiDotsSixVerticalBold, PiDotsThreeOutline } from 'react-icons/pi'
-import RoundedCheckbox from './RoundedCheckbox'
-import _ from '../../lib/lib'
-import { SlCalender } from 'react-icons/sl'
-import { GoPencil } from 'react-icons/go'
-import { MdOutlineDateRange } from 'react-icons/md'
-import { FaRegMessage } from 'react-icons/fa6'
-import { GetDateNow } from '../../utils/utils'
-import CalendarPopup from './CalenderPopup'
-import TaskPage from '../../pages/TaskPage/TaskPage'
-import { ref, set } from 'firebase/database'
-import { db } from '../../../Database/FirebaseConfig'
+import React, { useState } from "react";
+import { PiDotsSixVerticalBold, PiDotsThreeOutline } from "react-icons/pi";
+import RoundedCheckbox from "./RoundedCheckbox";
+import _ from "../../lib/lib";
+import { SlCalender } from "react-icons/sl";
+import { GoPencil } from "react-icons/go";
+import { MdOutlineDateRange } from "react-icons/md";
+import { FaRegMessage } from "react-icons/fa6";
+import { GetDateNow } from "../../utils/utils";
+import CalendarPopup from "./CalenderPopup";
+import TaskPage from "../../pages/TaskPage/TaskPage";
+import { ref, set } from "firebase/database";
+import { db } from "../../../Database/FirebaseConfig";
 
 const TaskCard = ({ taskData }) => {
   const [hover, setHover] = useState(false);
   const [recheduleMode, setRecheduleMode] = useState(false);
   const [date, setDate] = useState(taskData.date);
-  const [openTaskPage, setOpenTaskpage] = useState(false)
+  const [openTaskPage, setOpenTaskpage] = useState(false);
 
   const handleRechedule = async (taskId, selectedDate) => {
     const dateRef = ref(db, `tasks/${taskId}/date`);
-    const deadlineRef = ref(db, `tasks/${taskId}/deadline`)
+    const deadlineRef = ref(db, `tasks/${taskId}/deadline`);
     try {
-      await Promise.all([set(dateRef, selectedDate), set(deadlineRef, selectedDate)])
-      console.log('rescheduling successful')
+      await Promise.all([
+        set(dateRef, selectedDate),
+        set(deadlineRef, selectedDate),
+      ]);
+      console.log("rescheduling successful");
     } catch (error) {
-      console.error('Task recheduling failed ', error)
+      console.error("Task recheduling failed ", error);
     }
-  }
+  };
 
   return (
     <>
-      {
-        openTaskPage && <TaskPage taskData={taskData} setOpenTaskPage={setOpenTaskpage} />
-      }
-      <div className='flex justify-between items-start cursor-pointer pb-3' onMouseOver={() => setHover(true)} onMouseLeave={() => setHover(false)} >
-        <div className="left flex items-start" onClick={() => setOpenTaskpage(true)}>
+      {openTaskPage && (
+        <TaskPage taskData={taskData} setOpenTaskPage={setOpenTaskpage} />
+      )}
+      <div
+        className="flex justify-between items-start cursor-pointer pb-3"
+        onMouseOver={() => setHover(true)}
+        onMouseLeave={() => setHover(false)}
+      >
+        <div
+          className="left flex items-start"
+          onClick={() => setOpenTaskpage(true)}
+        >
           <div className="flex  gap-x-1 ">
             <div className="flex items-start translate-y-1">
-              <span className={`text-xl ${hover ? 'visible' : 'invisible'}`}>
+              <span className={`text-xl ${hover ? "visible" : "invisible"}`}>
                 <PiDotsSixVerticalBold />
               </span>
               <RoundedCheckbox />
@@ -49,38 +59,64 @@ const TaskCard = ({ taskData }) => {
                 <span>
                   <SlCalender />
                 </span>
-                <p className='text-fontSecondery'>{GetDateNow() === date ? 'Today' : Number(date.split(' ')[2]) - Number(GetDateNow().split(' ')[2]) === 1 ? 'Tomorrow' : date}</p>
+                <p className="text-fontSecondery">
+                  {GetDateNow() === date
+                    ? "Today"
+                    : Number(date.split(" ")[2]) -
+                        Number(GetDateNow().split(" ")[2]) ===
+                      1
+                    ? "Tomorrow"
+                    : date}
+                </p>
               </div>
             </div>
           </div>
         </div>
         <div className="right flex flex-col justify-center items-center">
-          <div className={`icons flex items-center gap-x-3 text-xl text-fontSecondery ${hover ? 'visible' : 'invisible'}`}>
-            <span className='text-2xl hover:text-accentMain' >
+          <div
+            className={`icons flex items-center gap-x-3 text-xl text-fontSecondery ${
+              hover ? "visible" : "invisible"
+            }`}
+          >
+            <span className="text-2xl hover:text-accentMain">
               <GoPencil />
             </span>
-            <span className='text-2xl hover:text-accentMain relative' onClick={() => setRecheduleMode(!recheduleMode)}>
+            <span
+              className="text-2xl hover:text-accentMain relative"
+              onClick={() => setRecheduleMode(!recheduleMode)}
+            >
               <MdOutlineDateRange />
-              <span className={`absolute top-10` + (recheduleMode ? ' visible' : ' invisible')}>
-                <CalendarPopup onSelect={selectedDate => {
-                  console.log(selectedDate.toDateString())
-                  const selectedDateStr = selectedDate.toDateString().split(' ').slice(0, 3).join(' ')
-                  setDate(selectedDateStr);
-                  handleRechedule(taskData.id, selectedDateStr)
-                }} />
+              <span
+                className={
+                  `absolute top-10` +
+                  (recheduleMode ? " visible" : " invisible")
+                }
+              >
+                <CalendarPopup
+                  onSelect={(selectedDate) => {
+                    console.log(selectedDate.toDateString());
+                    const selectedDateStr = selectedDate
+                      .toDateString()
+                      .split(" ")
+                      .slice(0, 3)
+                      .join(" ");
+                    setDate(selectedDateStr);
+                    handleRechedule(taskData.id, selectedDateStr);
+                  }}
+                />
               </span>
             </span>
-            <span className=' hover:text-accentMain'>
+            <span className=" hover:text-accentMain">
               <FaRegMessage />
             </span>
-            <span className=' hover:text-accentMain'>
+            <span className=" hover:text-accentMain">
               <PiDotsThreeOutline />
             </span>
           </div>
         </div>
       </div>
     </>
-  )
-}
+  );
+};
 
-export default TaskCard
+export default TaskCard;
