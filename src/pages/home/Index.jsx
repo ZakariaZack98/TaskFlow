7fb65381224777499871b2d6617fe940874a1be0
sabbir00921 <sabbir00921@gmail.com>
2025-05-07 04:17:48 +0600

    BoardView done

--- src/pages/home/Index.jsx
+++ src/pages/home/Index.jsx
@@ -12,6 +12,7 @@ const Inbox = () => {
   const [todaysTaskData, setTodaysTaskData] = useState([]);
   const [overdueData, setOverdueTaskData] = useState([]);
   const [getUpcomingTaskData, setUpcomingTaskData] = useState([]);
+  const [boardviewdata, setBoardviewdata] = useState([])
   const [boardview, setBoardview] = useState(false)
 
   useEffect(() => {
@@ -24,37 +25,54 @@ const Inbox = () => {
     setUpcomingTaskData(allTaskData.filter(task => task?.deadline > todayMs));
   }, [allTaskData]);
 
+  // Three types data are store in boardviewdata
+  useEffect(() => {
+    setBoardviewdata(
+      [{
+        name: "Overdue",
+        data: overdueData
+      },
+      {
+        name: "Today",
+        data: todaysTaskData
+      },
+      {
+        name: "Upcoming",
+        data: getUpcomingTaskData
+      }])
+  }, [overdueData, todaysTaskData, getUpcomingTaskData])
+
+
   return (
     <>
-      <div className={boardview?"heading w-6/10 mx-auto pb-5":"heading w-[90%] mx-auto pb-5"}>
+      <div className={boardview ? "heading w-6/10 mx-auto pb-5" : "heading w-[90%] mx-auto pb-5"}>
         <h1 className='text-3xl font-bold'>Inbox</h1>
         <AddTaskPrompt />
       </div>
-      {boardview?
-      <div className='h-full w-full overflow-y-scroll' style={{ scrollbarWidth: 'none' }}>
-        <div className="pendingTaskContainer w-6/10 mx-auto ">
-
-          <div className="taskList flex flex-col gap-y-3 my-3 ">
-            {
-              overdueData?.length > 0 && (
-                <TasklistSection title={'Overdue'} titleColorClass={'text-accentMain'} taskData={overdueData} />
-              )
-            }
-            {
-              todaysTaskData?.length > 0 && (
-                <TasklistSection title={'Today'} taskData={todaysTaskData} />
-              )
-            }
-            {
-              getUpcomingTaskData?.length > 0 && (
-                <TasklistSection title={'Upcoming'} taskData={getUpcomingTaskData} />
-              )
-            }
+      {boardview ?
+        <div className='h-full w-full overflow-y-scroll' style={{ scrollbarWidth: 'none' }}>
+          <div className="pendingTaskContainer w-6/10 mx-auto ">
+            <div className="taskList flex flex-col gap-y-3 my-3 ">
+              {
+                overdueData?.length > 0 && (
+                  <TasklistSection title={'Overdue'} titleColorClass={'text-accentMain'} taskData={overdueData} />
+                )
+              }
+              {
+                todaysTaskData?.length > 0 && (
+                  <TasklistSection title={'Today'} taskData={todaysTaskData} />
+                )
+              }
+              {
+                getUpcomingTaskData?.length > 0 && (
+                  <TasklistSection title={'Upcoming'} taskData={getUpcomingTaskData} />
+                )
+              }
+            </div>
           </div>
         </div>
-      </div>
-      :
-      <Boardview  taskData = {[overdueData,todaysTaskData,getUpcomingTaskData]}/>
+        :
+        <Boardview taskData={boardviewdata} />
       }
     </>
   );
