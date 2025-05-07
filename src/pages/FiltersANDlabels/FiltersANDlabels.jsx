b2d4fa38dc5804c24043fee865b8460ee344fae0
sabbir00921 <sabbir00921@gmail.com>
2025-05-07 19:47:07 +0600

    FiltersANDlabels Done

--- src/pages/FiltersANDlabels/FiltersANDlabels.jsx
+++ src/pages/FiltersANDlabels/FiltersANDlabels.jsx
@@ -21,16 +21,16 @@ const FiltersANDlabels = () => {
 
   return (
     <>
-      <div className="heading w-[90%] mx-auto pb-5">
-        <h1 className='text-3xl font-bold'>Inbox</h1>
+      <div className="heading w-[80%] mx-auto pb-5">
+        <h1 className='text-3xl font-bold'>Filters & Labels</h1>
         <AddTaskPrompt />
       </div>
-      <div className='bg-red-70 flex justify-center  items-center gap-2'>
-        <div className='flex justify-around h-screen  w-[90%] gap-5 '>
+      <div className='bg-red-70 flex justify-center items-center gap-2'>
+        <div className=' flex flex-wrap flex-col h-screen  w-[80%] gap-2 '>
           {filterData?.map(({ name, data }) => (
-            <div className='w-1/3 h-[70%] overflow-hidden '>
+            <div className='w-1/2 h-[35%] overflow-hidden shadow-sm rounded'>
               <h1 className={`text-2xl font-semibold border-b-2 pb-4 mb-2 border-[rgba(0,0,0,0.27)] py-1 text-black`}>{name}</h1>
-              <div className='flex flex-col gap-2 h-[88%] overflow-y-scroll overflow-x-hidden ' style={{ scrollbarWidth: "none" }}>
+              <div className='flex flex-col gap-2 h-[88%] overflow-y-scroll overflow-x-hidden px-5' style={{ scrollbarWidth: "none" }}>
                 {
                   data?.map((task, idx, arr) => {
                     return <div className='border rounded-lg border-[rgba(0,0,0,0.15)] '>
